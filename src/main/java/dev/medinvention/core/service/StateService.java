b492a37dc903ca9ab2381cb49cf2577bb502d688
Marouan MOHAMED <medmarouen@gmail.com>
2020-03-10 19:48:51 +0100

    Add pod/node/cluster service and controller

--- src/main/java/dev/medinvention/core/service/StateService.java
+++ src/main/java/dev/medinvention/core/service/StateService.java
@@ -22,8 +22,14 @@ public class StateService extends AbstractClient {
 		V1NodeList list = this.getAPI().listNode(null, null, null, null, null, null, null, null, null);
 
 		for (V1Node item : list.getItems()) {
+			if(item.getSpec() == null) {
+				continue;
+			}
 			for (V1Taint taint : item.getSpec().getTaints()) {
 				if (taint.getKey().contentEquals("node-role.kubernetes.io/master")) {
+					if(item.getStatus() == null) {
+						continue;
+					}
 					for (V1NodeCondition condition : item.getStatus().getConditions()) {
 						if (condition.getType().contentEquals("Ready") && condition.getStatus().contentEquals("True")) {
 							state.setIsRunning(true);
